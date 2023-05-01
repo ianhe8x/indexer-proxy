0e5ba47ce41e6787fb93a308d115862cfb766fc5
Ian He <39037239+ianhe8x@users.noreply.github.com>
2023-05-01 20:42:25 +1200

    test cache

--- .github/workflows/scripts/createdummy.sh
+++ .github/workflows/scripts/createdummy.sh
@@ -10,8 +10,8 @@ main() {
   cp utils/Cargo.toml .dummy/utils
   sed -i 's/^version = "[^"]*"$/version = "1.0.0"/' .dummy/proxy/Cargo.toml
   sed -i 's/^version = "[^"]*"$/version = "0.3.0"/' .dummy/utils/Cargo.toml
-  touch .dummy/proxy/src/main.rs
-  touch .dummy/utils/src/lib.rs
+  echo "fn main() {}" > .dummy/proxy/src/main.rs
+  echo "fn main() {}" > .dummy/utils/src/lib.rs
 }
 
 main "$@"
