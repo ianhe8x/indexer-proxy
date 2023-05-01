8f5f4cae8e0a2e1faca9bb3527e96bc6c635a99d
Ian He <39037239+ianhe8x@users.noreply.github.com>
2023-05-01 17:42:16 +1200

    test cache

--- .github/workflows/scripts/createdummy.sh
+++ .github/workflows/scripts/createdummy.sh
@@ -6,8 +6,8 @@ main() {
   mkdir -p .dummy/utils/src
   mkdir -p .dummy/proxy/src
   cp Cargo.toml Cargo.lock .dummy/
-  cp proxy/Cargo.toml .dummy/proxy/src
-  cp utils/Cargo.toml .dummy/utils/src
+  cp proxy/Cargo.toml .dummy/proxy
+  cp utils/Cargo.toml .dummy/utils
   sed -i 's/^version = "[^"]*"$/version = "1.0.0"/' .dummy/proxy/src/Cargo.toml
   sed -i 's/^version = "[^"]*"$/version = "0.3.0"/' .dummy/utils/src/Cargo.toml
   touch .dummy/proxy/src/main.rs
