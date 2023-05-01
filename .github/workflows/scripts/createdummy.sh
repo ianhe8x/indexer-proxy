5610e3faa0e816c817862e07572bdc2706350038
Ian He <39037239+ianhe8x@users.noreply.github.com>
2023-05-01 17:51:51 +1200

    test cache

--- .github/workflows/scripts/createdummy.sh
+++ .github/workflows/scripts/createdummy.sh
@@ -3,15 +3,15 @@
 set -euo pipefail
 
 main() {
-  mkdir -p .dummy/utils/src
-  mkdir -p .dummy/proxy/src
-  cp Cargo.toml Cargo.lock .dummy/
-  cp proxy/Cargo.toml .dummy/proxy
-  cp utils/Cargo.toml .dummy/utils
-  sed -i 's/^version = "[^"]*"$/version = "1.0.0"/' .dummy/proxy/Cargo.toml
-  sed -i 's/^version = "[^"]*"$/version = "0.3.0"/' .dummy/utils/Cargo.toml
-  touch .dummy/proxy/src/main.rs
-  touch .dummy/utils/src/lib.rs
+  mkdir -p dummy/utils/src
+  mkdir -p dummy/proxy/src
+  cp Cargo.toml Cargo.lock dummy/
+  cp proxy/Cargo.toml dummy/proxy
+  cp utils/Cargo.toml dummy/utils
+  sed -i 's/^version = "[^"]*"$/version = "1.0.0"/' dummy/proxy/Cargo.toml
+  sed -i 's/^version = "[^"]*"$/version = "0.3.0"/' dummy/utils/Cargo.toml
+  touch dummy/proxy/src/main.rs
+  touch dummy/utils/src/lib.rs
 }
 
 main "$@"
